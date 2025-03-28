cbb9a664728be6dd69d855515112b06baf67850c
Javier Flores <javerflors@gmail.com>
2025-03-28 10:47:55 -0600

    fix: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager - Head */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-5C6VFTCQ';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5C6VFTCQ');
+          `}
+        </Script>
+      </head>
       <body className={`${roboto.variable} antialiased bg-primary`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5C6VFTCQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {/* Google Analytics Scripts */}
         <Script
           strategy="afterInteractive"
